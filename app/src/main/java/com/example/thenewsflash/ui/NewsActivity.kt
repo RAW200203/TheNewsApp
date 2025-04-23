8617848095d9efec5802751204e1812c46d3f778
RAW200203 <randyachana2002@gmail.com>
2025-04-23 20:27:53 +0200

    progressing!!!!!!

--- app/src/main/java/com/example/thenewsflash/ui/NewsActivity.kt
+++ app/src/main/java/com/example/thenewsflash/ui/NewsActivity.kt
@@ -6,18 +6,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsprojectpractice.R
+import com.example.newsprojectpractice.databinding.ActivityNewsBinding
+import com.example.thenewsflash.db.ArticleDatabase
+import com.example.thenewsflash.models.Article
+import com.example.thenewsflash.repository.NewsRepository
 
 class NewsActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var binding: ActivityNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_news)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_newsActivity)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        newsViewModel = ViewModelProvider(this,viewModelProviderFactory).get(NewsViewModel::class.java)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
+
+//        enableEdgeToEdge()
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_newsActivity)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
